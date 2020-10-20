a2a47fd6890f326e38b7d5bd985d1777e90cbbda
Hauke97 <72126928+Hauke97@users.noreply.github.com>
2020-10-20 12:52:45 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HSP_Zahnrad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//hallo
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
