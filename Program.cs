e22d6fe9935a94e11bc7b361813835dcf53f2d69
Hauke97 <72126928+Hauke97@users.noreply.github.com>
2020-10-21 15:23:58 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 
-namespace HSP_Zahnrad
+namespace Zahnrad2
 {
     class Program
     {
         static void Main(string[] args)
-        {//hallo
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Zahnrad");
+        {
+            Console.WriteLine("Geben Sie den Modul ein");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben Sie die Zähnezahl ein");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben Sie den Teilkreisdurchmesser ein");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben Sie die Breite ein");
+            double b = Convert.ToDouble(Console.ReadLine());
+
         }
     }
 }
